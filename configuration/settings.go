b88a5c365fe88504808fd96ad6bc59f613a4a321
deeper-x <albertodeprezzo@gmail.com>
2020-03-05 12:59:19 +0100

    codacy fixes

--- configuration/settings.go
+++ configuration/settings.go
@@ -14,5 +14,5 @@ var ConnString = fmt.Sprintf("amqp://guest:guest@%s/", RabbitmqURI)
 // MonitoredDir directory
 var MonitoredDir = "/tmp/monitor"
 
-// AwsTopic
+// AwsTopic is the default topic (is private, will be hidden soon though)
 var AwsTopic = "arn:aws:sns:eu-west-3:777350386990:justopic"
