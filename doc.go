93c215da96b35ef0b7fcc8c02b53c62271ca236f
deeper-x <albertodeprezzo@gmail.com>
2020-03-19 21:47:48 +0100

    added doc.go

---
+++ doc.go
@@ -0,0 +1,5 @@
+/*
+System listens for file creation in a monitored directory, producing content parsing, reading and sending to a receiver (is a typical Producer->Consumer model with queue, handled by a monitoring agent).
+
+*/
+package main
